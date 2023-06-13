84130aed0ed726e1e49072734ff0ead8ef1d3b7f
Шворак Віталій <136110641+ShvorakVitaliy@users.noreply.github.com>
2023-06-13 15:55:02 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String... args) {
+        int[] array = {999, 88, -9, -20, 159, 753, 123, 4, 6, 1, 0};
+        sort(array);
+        System.out.println(Arrays.toString(array));
     }
+
+    private static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if(array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
 }
